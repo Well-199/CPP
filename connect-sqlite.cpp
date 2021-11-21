c6fcd370024990e8bfa86fcbb74a03c0b5e1fc3d
Wellington Lira <wellington.marunaka@gmail.com>
2021-11-21 20:21:20 -0300

    0.1

---
+++ connect-sqlite.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Ola Mundo" << '\n';
+    return 0;
+}
---
+++ construtor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Qualquer {
+    public:
+        Qualquer(){
+            cout << "Qualquer Coisa";
+        }
+};
+
+int main(){
+    Qualquer q;
+
+    return 0;
+}
